da90c6a48cb7f33aff3de715d504cace5dbee73f
mahdiyar.th <mahdiyart@gmail.com>
2025-07-20 22:42:52 +0330

    feat: Add execution time for repositories and services

---
+++ src/main/java/com/azki/config/ExecutionTimeAspect.java
@@ -0,0 +1,28 @@
+package com.azki.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class ExecutionTimeAspect {
+
+    @Around("execution(* *(..)) && (within(com.azki..*Repository*) || within(com.azki..*Service*))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long duration = System.currentTimeMillis() - start;
+
+        if (duration < 50) {
+            log.info("Method {} executed in {} ms", joinPoint.getSignature(), duration);
+        } else {
+            log.warn("Method {} executed in {} ms", joinPoint.getSignature(), duration);
+        }
+
+        return proceed;
+    }
+}
